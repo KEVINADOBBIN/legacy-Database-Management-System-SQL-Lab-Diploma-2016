e9019594a5ce3928a8589260f3e9b5c70b7c56d2
Kevin A Dobbin <94518887+KEVINADOBBIN@users.noreply.github.com>
2026-01-08 19:38:43 +0530

    Add files via upload

---
+++ select_statement.sql
@@ -0,0 +1,16 @@
+-- =========================================
+-- ILLUSTRATE THE USE OF SELECT STATEMENT
+-- REGISTER NUMBER: 408CS15015
+-- =========================================
+
+-- 1. Retrieve the details of all employees
+SELECT * FROM employee123;
+
+-- 2. Retrieve the Fname and Lname of all employees
+SELECT fname, lname FROM employee123;
+
+-- 3. Retrieve the salary of all employees
+SELECT salary FROM employee123;
+
+-- 4. Retrieve the distinct salary of all employees
+SELECT DISTINCT salary FROM employee123;
